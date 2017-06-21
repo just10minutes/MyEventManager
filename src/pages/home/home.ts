6763089e913e2ba3031339287c9d550dedeb0462
Rajesh <just10minutes@gmail.com>
2017-06-21 18:58:47 +0200

    tutorial_2

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,9 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  constructor(public navCtrl: NavController) {}
 
-  constructor(public navCtrl: NavController) {
-
+  goToProfile(){ 
+    this.navCtrl.push('profile'); 
   }
-
 }
